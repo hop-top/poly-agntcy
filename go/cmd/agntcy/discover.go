943c39027fc1cc4a336ee7391af06830111ca59c
Jad Bitar <jadb@users.noreply.github.com>
2026-05-30 05:13:56 -0400

    feat(go): add agntcy CLI with register discover describe publish verify

---
+++ go/cmd/agntcy/discover.go
@@ -0,0 +1,37 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"hop.top/agntcy/dir"
+)
+
+func newDiscoverCmd() *cobra.Command {
+	var capability string
+	c := &cobra.Command{
+		Use:   "discover",
+		Short: "Discover agents by capability",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			endpoint, _ := cmd.Flags().GetString("endpoint")
+			client, err := dir.NewClient(dir.Options{
+				Endpoint:    endpoint,
+				Credentials: dir.InsecureCredentials{},
+			})
+			if err != nil {
+				return err
+			}
+			res, err := client.Discover(context.Background(), dir.DiscoverParams{Capability: capability})
+			if err != nil {
+				return err
+			}
+			for _, a := range res.Agents {
+				fmt.Fprintln(cmd.OutOrStdout(), a.GetName())
+			}
+			return nil
+		},
+	}
+	c.Flags().StringVar(&capability, "capability", "", "Capability to search for")
+	return c
+}
---
+++ go/cmd/agntcy/register.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/spf13/cobra"
+	"hop.top/agntcy/dir"
+)
+
+func newRegisterCmd() *cobra.Command {
+	var name string
+	var endpointURL string
+	c := &cobra.Command{
+		Use:   "register",
+		Short: "Register an agent descriptor",
+		RunE: func(cmd *cobra.Command, _ []string) error {
+			endpoint, _ := cmd.Flags().GetString("endpoint")
+			client, err := dir.NewClient(dir.Options{
+				Endpoint:    endpoint,
+				Credentials: dir.InsecureCredentials{},
+			})
+			if err != nil {
+				return err
+			}
+			res, err := client.Register(context.Background(), dir.RegisterParams{
+				Agent: &dir.AgentDescriptor{Name: name, Endpoint: endpointURL},
+			})
+			if err != nil {
+				return err
+			}
+			fmt.Fprintln(cmd.OutOrStdout(), res.ID)
+			return nil
+		},
+	}
+	c.Flags().StringVar(&name, "name", "", "Agent name")
+	c.Flags().StringVar(&endpointURL, "agent-endpoint", "", "Agent endpoint URL")
+	return c
+}
